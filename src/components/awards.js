539e316f577603ab288a058c6d9ffcde4217c12e
Isaias Fernandez <isafernande04@gmail.com>
2023-01-17 17:29:46 -0300

    actualizacion de reconocimientos, adicion de links a los reconocimientos, arreglo de respnsive de secciones en general, responsive para dispositivos pequeños, y configuracion del wrap en reconocimientos y proyectos personales

--- src/components/awards.js
+++ src/components/awards.js
@@ -8,7 +8,7 @@ function AwardsSections() {
       <div className="reconocimientos">
         <Reconocimento
           title="Expositor en EXPOCYTAR Web 2022"
-          url="/"
+          url="https://www.expocytar.com/"
           logo="https://i.imgur.com/NnbkLA8.png"
         />
         <Reconocimento
@@ -18,18 +18,28 @@ function AwardsSections() {
         />
         <Reconocimento
           title="Joven Destacado 2021 por el gobierno de la provincia de Salta"
-          url="/"
+          url="https://www.facebook.com/subsecjuventud.delaprovinciadesalta/posts/pfbid02RXDd7fvi3KYjkXpzovvvnAcG4JSm4diTWudzz55Dp7wzYjdbH9b4mmJnabKSdRpZl"
           logo="https://i.imgur.com/UwM6Mxn.png"
         />
         <Reconocimento
           title="Reconocimiento por desarollo innovador por la secretaria de logística de San Luis"
-          url="/"
+          url="https://agenciasanluis.com/notas/2021/07/23/reconocieron-a-dos-equipos-que-participaron-del-innovaton-san-luis-2021/?fbclid=IwAR3Yl5P3XeDRN5O4NDyDmmAcD5tDVCQWvJLaWjtooR8sDvSoZaeHPovHNeA"
           logo="https://i.imgur.com/b2DUc7K.png"
         />
         <Reconocimento
           title="Reconocimiento por innovaciones informáticas en La Escuela Técnica N° 3141 Dimitri Mendeleiev"
-          url="/"
+          url="https://www.facebook.com/photo/?fbid=1742948362727890&set=a.795192014170201"
           logo="https://i.imgur.com/7ymG1mA.png"
+        />
+        <Reconocimento
+          title="Ganador del Desafio Emprender Coca Cola 2022"
+          url="https://prnoticias.com/2022/11/15/estos-son-los-ganadores-del-desafio-emprender-coca-cola-en-la-argentina/#:~:text=Puntualmente%2C%20el%20programa%20Emprendidos%20busca,digital%20y%20el%20e%2Dcommerce."
+          logo="https://i.imgur.com/b218ME9.png"
+        />
+        <Reconocimento
+          title="Mención de Honor Baca Instituto Balseiro 2022"
+          url="https://www.ib.edu.ar/resultados.html"
+          logo="https://i.imgur.com/cVlQLMC.png"
         />
       </div>
     </section>
